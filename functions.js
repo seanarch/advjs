0f83ad6c3ecdb344851eb74c4f8252f6a3efa2ca
Sean <zhuqingyun@outlook.com>
2022-01-10 22:04:07 -0700

    spread operator

--- functions.js
+++ functions.js
@@ -5,3 +5,19 @@ function greetUser(greetingPrefix, userName = 'user') {
 greetUser('Hi', 'Max');
 greetUser('Hello');
 greetUser('Hello', 'John');
+
+
+function sumUp(...numbers) {
+    let result = 0;
+
+    for (const number of numbers) {
+        result += number;
+    }
+
+    return result;
+}
+
+console.log(sumUp(1, 5, 10, 11, 20, 31));
+
+const inputNumbers = [1, 5, 10, 11, 20, 31];
+console.log(sumUp(...inputNumbers));
