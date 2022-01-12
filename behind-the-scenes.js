ff0ed2a4df3821d9a254569fb84bed39a6807dfc
Sean <zhuqingyun@outlook.com>
2022-01-11 21:04:15 -0700

    spread object property

--- behind-the-scenes.js
+++ behind-the-scenes.js
@@ -1,9 +1,20 @@
-const hobbies = ['Sports', 'Cooking'];
-const age = 32;
+const hobbies = ['Sports', 'Cooking']; // a pointer to the array is stored 
+const age = 32; // the value itself is stored
 
-hobbies.push('Reading');
+hobbies.push('Reading'); // the address of the array doesn't change
 
 console.log(hobbies);
 
 // primitive values: numbers, strings, booleans & more (undefined)
 // reference values: objects 
+
+const person = { age: 32 };
+
+function getAdultYears(p) {
+    //p.age = p.age - 18;
+
+    return p.age - 18;
+}
+
+console.log(getAdultYears({ ...person }));
+console.log(person);
