77c79a922292e3f69e607bc56a222547a438d9ce
Sean <zhuqingyun@outlook.com>
2022-01-11 19:08:58 -0700

    data types

---
+++ behind-the-scenes.js
@@ -0,0 +1,9 @@
+const hobbies = ['Sports', 'Cooking'];
+const age = 32;
+
+hobbies.push('Reading');
+
+console.log(hobbies);
+
+// primitive values: numbers, strings, booleans & more (undefined)
+// reference values: objects 
