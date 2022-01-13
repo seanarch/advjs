1784f5dfe5d93b33d29e0dec6cac0dca0533ebcc
Sean <zhuqingyun@outlook.com>
2022-01-12 23:16:54 -0700

    add describe

--- objects.js
+++ objects.js
@@ -9,9 +9,16 @@ class Job {
         this.location = place;
         this.salary = salary;
     }
+
+    describe() {
+        console.log(`I'm a ${this.title}, I work in ${this.location}, and I earn ${this.salary}.`);
+    }
 }
 
 const developer = new Job('Developer', 'New York', 50000);
 const cook = new Job('Cook', 'Munich', 35000);
 console.log(developer);
 console.log(cook);
+
+developer.describe();
+cook.describe();
