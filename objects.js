28f8c18ea6fc5b6e2bebbb7d8ae67639224cc30c
Sean <zhuqingyun@outlook.com>
2022-01-12 23:12:13 -0700

    New object

--- objects.js
+++ objects.js
@@ -1,6 +1,15 @@
-const job = { title: 'Developer', location: 'New York', salary: 50000, }
+// const job = { title: 'Developer', location: 'New York', salary: 50000, }
 
 
+// console.log(new Date().toISOString); 
 
+class Job {
+    constructor(jobTitle, place, salary) {
+        this.title = jobTitle;
+        this.location = place;
+        this.salary = salary;
+    }
+}
 
-console.log(new Date().toISOString); 
+const developer = new Job('Developer', 'New York', 50000);
+console.log(developer);
